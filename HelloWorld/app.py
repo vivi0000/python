03006a780496d600c3bf3b2ed8d58ed52c8029d4
vivi0000 <evo.dyl@qq.com>
2020-07-08 23:17:23 +0800

    learn python day 2

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -96,11 +96,248 @@
 #     print("It's a lovely day")
 # print("Enjoy your day")
 
-price = 1000000
-is_good = True
+# price = 1000000
+# is_good = True
+#
+# if is_good:
+#     price = price * 0.1
+# else:
+#     price = price * 0.2
+# print(price)
 
-if is_good:
-    price = price * 0.1
-else:
-    price = price * 0.2
-print(price)
+# has_high_income = False
+# has_good_credit = True
+# has_criminal_record = False
+
+# if has_high_income and has_good_credit:
+#     print("Eligible for loan")
+
+# if has_high_income or has_good_credit:
+#     print("Eligible for loan")
+
+# if has_good_credit and not has_criminal_record:
+#     print("Eligible for loan")
+
+# temperature  = 30
+#
+# if temperature <= 30:
+#     print("It's a hot day")
+
+# name = input("name: ")
+# if len(name) < 3:
+#     print("name must be at least 3 characters")
+# elif len(name) > 50:
+#     print("name can be a maximum of 50 characters")
+# else:
+#     print("name looks good!")
+
+# weight = int(input('Weight: '))
+# unit = input('(L)bs or (K)g: ')
+# if unit.upper() == "L":
+#     converted = weight * 0.45
+#     print(f"You are {converted} kilos")
+# else:
+#     converted = weight / 0.45
+#     print(f"You are {converted} pounds")
+
+# is_run = False
+# while True:
+#     order = input("> ").lower()
+#     if order == 'help':
+#         print('start - to start the car')
+#         print('stop - to stop the car')
+#         print('quit - to exit')
+#     elif order == 'start':
+#         if is_run == False:
+#             print('Car statrted... Ready to go!')
+#             is_run = True
+#         else:
+#             print('Car is already statrted')
+#     elif order == 'stop':
+#         if is_run == True:
+#             print('Car stopped...')
+#             is_run = False
+#         else:
+#             print('Car is already stopped.')
+#     elif order == 'quit':
+#         break
+#     else:
+#         print("I don't understand that...")
+
+# for item in 'Python':
+#     print(item)
+
+# for item in [1,2,3,4]:
+#     print(item)
+
+# for item in range(5,10,2):
+#     print(item)
+
+# prices = [10, 20, 30]
+# sum = 0
+# for item in prices:
+#     sum +=  item
+# print(f"Total:{sum}")
+
+# for循环
+# for x in range(4):
+#     # print(x)
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+# numbers = [5, 2, 5, 2, 2]
+# for num in numbers:
+#     # print('x'*num)
+#     output = ''
+#     for i in range(num):
+#         output+='x'
+#     print(output)
+
+# 列表
+# numbers = [5,2,1,5,7,4]
+# numbers.append(20)
+# numbers.insert(0,10)
+# numbers.remove(5)
+# # numbers.clear()
+# numbers.pop()
+# print(numbers)
+# print(numbers.index(5))
+# numbers.sort()
+# numbers.reverse()
+# numbers2 = numbers.copy()
+# numbers.sort()
+# print(numbers)
+# print(numbers2)
+
+# numbers = [2,2,3,4,3,4,6,1]
+# for item in numbers:
+#     if numbers.count(item)>1:
+#         numbers.remove(item)
+# print(numbers)
+
+# 解压缩
+# coordinates = (1,2,3)
+# x,y,z = coordinates#元祖列表均可使用
+# print(x)
+
+# 字典
+# costumer = {
+#     "name":"dyl",
+#     "age":30,
+#     "is_verified":True
+# }
+# costumer["name"] = 'dyll'
+# print(costumer["name"])
+# print(costumer.get("birthdate","Jan 1 1980"))
+# # 不会报错，没有会返回默认值
+
+#字符串拆分,替换
+# message = input("> ")
+# words = message.split(' ')
+# emojis = {
+#     ":)":"😊",
+#     ":(":"😒"
+# }
+# output = ''
+# for word in words:
+#     output += emojis.get(word,word)+" "
+# print(output)
+
+# 函数定义
+# def greet_user(first_name,last_name):
+# #     print(f"Hi {first_name} {last_name}!")
+# #     print("Welcome aboard")
+# #
+# # print("Start")
+# # greet_user("John","Smith")
+# # greet_user(last_name="Mary",first_name="aaa")
+# # print("Finish")
+
+# def square(number):
+#     return number * number
+#
+# # result = square(3)
+# # print(result)
+# print(square(3))
+
+# def emoji_converter(message):
+#     words = message.split(' ')
+#     emojis = {
+#         ":)":"😊",
+#         ":(":"😒"
+#     }
+#     output = ''
+#     for word in words:
+#         output += emojis.get(word,word)+" "
+#     return output
+# message = input("> ")
+# output = emoji_converter(message)
+# print(output)
+
+# 异常处理
+# try:
+#     age = int(input("Age: "))
+#     income = 20000
+#     risk = income/age
+#     print(age)
+# except ZeroDivisionError:
+#     print('Age cannot be 0')
+# except ValueError:
+#     print('Invalid value')
+
+# Class
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def mov(self):
+#         print("move")
+#
+#     def draw(self):
+#         print("draw")
+
+# point1 = Point(10,20)
+# point1.x = 11
+# # point1.y = 20
+# print(point1.x)
+# # point1.draw()
+
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def talk(self):
+#         print(f"Hi, I am {self.name}")
+#
+# dyl = Person("dyl")
+# dyl.talk()
+#
+# bob = Person("Bob")
+# bob.talk()
+
+# 继承
+# class Mammal:
+#     def walk(self):
+#         print("walk")
+#
+# class Dog(Mammal):
+#     def bark(self):
+#         print("bark")
+#
+# class Cat(Mammal):
+#     def be_annoying(self):
+#         print("annoying")
+#
+# dog1 = Dog()
+# dog1.walk()
+# cat1 = Cat()
+# cat1.be_annoying()
+
+# 模块
+# import converters
+# from converters import kg_to_lbs
+#
+# print(kg_to_lbs(100))
+#
+# print(converters.kg_to_lbs(70))
